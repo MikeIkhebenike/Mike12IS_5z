1910181d21621db6ea8ef6cf6e5f1a5da51c112d
Ikhebenike Mike <63050419+MikeIkhebenike@users.noreply.github.com>
2020-05-28 22:27:16 +0300

    Добавьте файлы проекта.

---
+++ Mike12IS_5z/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mike12IS_5z
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a, b, h,p, z;
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+            h = Convert.ToDouble(textBox3.Text);
+            p = (Math.Pow(a, 2) + Math.Pow(b, 2)) *3;
+            z =(Math.PI * h/ 6) * (p+Math.Pow(h,2));
+            listBox1.Items.Add("z = "+z+);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
